7e4d382b03254455400d1fbcad0ccf65dc043136
sholohov <asolohov@gmail.com>
2016-04-01 14:39:04 +0300

    create

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*
+ *		margin header
+ */
+
+var toolbar = document.querySelector("#toolbar");
+var header = document.querySelector("#header");
+header.style.marginTop = toolbar.offsetHeight + "px";
+
+/*
+*		
+*/
+
