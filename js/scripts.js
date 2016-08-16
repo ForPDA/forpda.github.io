9949dc99a54111c4f6086899304655c8ce212282
Alexey Sholohov <asolohov@gmail.com>
2016-08-16 12:53:30 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,36 @@
  */
 
 var body = document.body;
-body.style.height = document.documentElement.clientHeight + "px";
+// body.style.height = document.documentElement.clientHeight + "px";
 
 /*
-*		
-*/
+ *		Gallery	
+ */ 
 
+var gallery = document.querySelector('.gallery > div');
+
+var massPic = [
+	'light.png',
+	'dark.png',
+	'black.png',
+	'material_light.png',
+	'material_dark.png',
+	'material_black.png',
+	'white_old.png'
+];
+
+for (var i = 0; i < massPic.length; i++) {
+	var pic = massPic[i];
+	var a = document.createElement('a');
+	var img = document.createElement('img');
+	a.href = 'src/' + pic;
+	img.src = 'src/mini-' + pic;
+	img.alt = pic;
+	a.appendChild(img);
+	gallery.appendChild(a);
+
+}
+
+gallery.addEventListener('click', function() {
+//	alert(event.target.parentNode.innerHTML);
+});
